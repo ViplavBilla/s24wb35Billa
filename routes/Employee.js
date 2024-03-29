fce7b561bcbc0e788daa563d9953bdf61cb16540
ViplavBilla <156365246+ViplavBilla@users.noreply.github.com>
2024-03-29 12:38:35 -0500

    Update Employee.js

--- routes/Employee.js
+++ routes/Employee.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Employee', { title: 'Search Results Employee' });
+  res.render('employee', { title: 'Search Results Employee' });
 });
 
 module.exports = router;
